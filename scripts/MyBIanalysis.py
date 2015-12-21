65f7d98a4c9c910e4e79224c6f4d201b6dd0fdda
shiori <shiori@hikichi-no-MacBook-Air.local>
2015-12-21 20:42:28 +0900

    update

---
+++ scripts/MyBIanalysis.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# -----------------------
+# Last Update: 2015-12-21
+# -----------------------
+
+import os,commands,csv,sqlite3
+from sys import argv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def help():
+    '''help message'''
+    print ""
+    #print "Usage: %s [input_file]" %argv[0]
+    print "Usage: %s [program_file]" %argv[0]
+    print "Usage: %s [csv_file]" %argv[1]
+    print ""
+    print "This command eliminates something... in the input file."
+    sys.exit(1)
+
+def MyBIanalysis(anInputFile):
+
+    anFile1List = []
+
+    with open(anInputFile, "r") as File1:
+        anReader1 = csv.reader(File1)
+        anHeader1Str = next(anReader1)
+        for row in anReader1:
+            if len(row) == 4:
+                anFile1List.append(row)
+
+    SQLdataArray = np.array(anFile1List)
+
+# sql data
+
+#    SQLdataArray = np.c_[anFile1Array]
+#    print SQLdataArray
+
+# sqlite create table/ insert value
+
+    conn = sqlite3.connect(":memory:")
+    c = conn.cursor()
+    c.execute('''DROP TABLE IF EXISTS MyBIanalysisDB;''')
+    c.execute('''CREATE TABLE MyBIanalysisDB(
+        Kingdom text,
+        Groupname text,
+        SubGroupname text,
+        Size_Mb float
+        );''')
+
+# sqlite insert
+
+    def sqlinsert(table_name,data):
+        for row in data:
+            ph = "?," * (len(row) - 1) + "?"
+            conn.execute("INSERT INTO %(table_name)s VALUES(%(ph)s)" % locals(), tuple(row))
+    sqlinsert("MyBIanalysisDB", SQLdataArray)
+
+    c.execute("SELECT Kingdom,COUNT(*) FROM MyBIanalysisDB GROUP BY Kingdom;")
+    print "\n===== Input Kingdom count ====="
+    InputKingdomList = []
+    for row in c:
+        InputKingdomList.append([str(unicode(row[0])),row[1]])
+        print str(unicode(row[0])) + ", " + str(row[1])
+
+    c.execute("SELECT Groupname,COUNT(*) FROM MyBIanalysisDB GROUP BY Groupname;")
+    print "\n===== Input Groupname count ====="
+    InputGroupnameList = []
+    for row in c:
+        InputGroupnameList.append([str(unicode(row[0])),row[1]])
+        print str(unicode(row[0])) + ", " + str(row[1])
+
+    def bargraphfunction(xyList):
+        xList = []
+        xnameList = []
+        yList = []
+        xele = 0
+        for a in range(0,len(xyList)):
+            xList.append(xele)
+            xele += 0.5
+            xnameList.append(xyList[a][0])
+            yList.append(xyList[a][1])
+        plt.bar(xList,yList,width=0.4,align="center")
+        plt.ylabel("Number of metadata",fontsize=18)
+        plt.xticks(xList,xnameList,fontsize=15)
+        plt.yticks(fontsize=15)
+
+    plt.figure('Input Kingdom count')
+    plt.xlabel("Kingdom",fontsize=18)
+    bargraphfunction(InputKingdomList)
+
+    '''
+    print "Kingdom"
+    #a = commands.getoutput('pwd')
+    os.system('pwd')
+    os.chdir("../../ncbiGenomeList/data/")
+    os.system('grep -v "^#" overview.csv | cut -d, -f2 | sort | uniq -c')
+    a = commands.getoutput('grep -v "^#" overview.csv | cut -d, -f2 | sort | uniq -c')
+    print "ans\n" + str(a)
+    '''
+
+if __name__=='__main__':
+    if len(argv)!=2:
+        help()
+    MyBIanalysis(argv[1])
+    plt.show()
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
-# ---------------------
-# Last Update: 2015-12-16
-# ---------------------
+# -----------------------
+
+# Last Update: 2015-12-21
+
+# -----------------------
 
 #!/bin/bash
 set -euo pipefail
@@ -9,12 +11,18 @@ set -euo pipefail
 #python atgc.py
 
 cd ../../ncbiGenomeList
+#awk -F "\t" '{ print $2 "," $3 "," $4 "," $5 }' data/overview.txt > data/overview_size.csv
 
 echo "Kingdom"
 #grep -v "^#" data/overview.txt | cut -f2 | sort | uniq -c > ../BImyproject/analysis/kingdom.txt
-grep -v "^#" data/overview.csv | cut -d, -f2 | sort | uniq -c
-tr '  ' ',' <../BImyproject/analysis/kingdom.txt> ../BImyproject/analysis/kingdom.csv
-echo $'\nBacteria'
+grep -v "^#" data/overview.csv | cut -d, -f2 | sort | uniq -c | awk '{ print $0,$1 }'
+#tr '  ' ',' <../BImyproject/analysis/kingdom.txt> ../BImyproject/analysis/kingdom.csv
+echo $'\nBacteria (Genus)'
 #grep "Bacteria" data/overview.txt | cut -f3,4 | sort | uniq -c > ../BImyproject/analysis/bacteria.txt
-grep "Bacteria" data/overview.csv | cut -d, -f3,4 | sort | uniq -c
+grep "Bacteria" data/overview.csv | cut -d, -f3 | sort | uniq -c
 #tr '  ' ',' <../BImyproject/analysis/bacteria.txt> ../BImyproject/analysis/bacteria.csv
+echo $'\nBacteria (Species)'
+grep "Bacteria" data/overview.csv | cut -d, -f3,4 | sort | uniq -c
+
+cd ../BImyproject/scripts
+python MyBIanalysis.py ../../ncbiGenomeList/data/overview_size.csv
